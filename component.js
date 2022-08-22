52fd586efacefc4ee98af6f2fc5010967e11635b
vidya504 <109609828+vidya504@users.noreply.github.com>
2022-08-22 21:37:12 +0530

    Add files via upload

---
+++ component.js
@@ -0,0 +1,12 @@
+<html>
+<body>import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+class Car extends React.Component {
+  render() {
+    return <h2>I am a {this.props.color} Car!</h2>;
+  }
+}
+
+ReactDOM.render(<Car color="red"/>, document.getElementById('root'));
+</body>
